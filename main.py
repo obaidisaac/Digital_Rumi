ac574f0b3b1015791c8569dffe514328e07560a3
obaidisaac <146994155+obaidisaac@users.noreply.github.com>
2024-11-12 18:23:40 -0600

    changes for efficiency

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-#import sqlite3
+import sqlite3
 
 #from langchain.llms import HuggingFaceHub
 #from langchain.document_loaders import TextLoader
